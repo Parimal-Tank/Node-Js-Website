eb15c5c0a7b7f34b586d5cd3d40222e27af3321d
Parimal-Tank <parimalt@zignuts.com>
2023-01-09 16:21:28 +0530

    template Engines , Partial Templates , Middleware and Statics File , Query String , Handaling Post Requests

--- app.js
+++ app.js
@@ -1,18 +1,54 @@
 const express = require('express');
-
 const app = express();
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false})
 
+app.set('view engine' , 'ejs');
 app.listen(3000);
 
 
+//example of middleware implementation 
+// app.use('/assets' , function(req,res , next){
+//     console.log(req.url);
+//     next();
+// })
+
+// middleware using express 
+app.use('/assets' , express.static('assets'));   // use to map with static files
+
+// using express methods
+
+// app.get('/' , function(req , res){
+//     res.sendFile(__dirname + '/index.html');
+// })  
+
+// app.get('/contact',function(req, res){
+//     res.sendFile(__dirname + '/contact.html');
+// })
+
+
+// app.get('/profile/:id' , function(req , res){
+//     res.send("You requested to see profile with the id" + req.params.id);
+// })
+
+// using EJS to view their contant
+
 app.get('/' , function(req , res){
-    res.sendFile(__dirname + '/index.html');
+    res.render('index');
 })  
 
 app.get('/contact',function(req, res){
-    res.sendFile(__dirname + '/contact.html');
+    // console.log(req.query); // Example of Query String
+    res.render('contact' , {qs: req.query});
 })
 
-app.get('/profile/:id' , function(req , res){
-    res.send("You requested to see profile with the id" + req.params.id);
+app.post('/contact', urlencodedParser ,function(req, res){
+    console.log(req.body);
+    res.render('contact-success' , {data : req.body});
 })
+
+app.get('/profile/:name' , function(req , res){
+    const data = {age : 21 , job: 'Web Developer' , hobbies : ['Reading' , 'Outdoor Games' , 'Traveling']};
+        res.render('profile' , {person: req.params.name , data : data});
+})
+
